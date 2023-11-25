02b9e38fb1041189936b70c3eda81d56bf7ab076
ultr4 <cl.rch09@icloud.com>
2023-11-25 15:12:07 -0300

    :sparkles: feat: Created a RequireAuth func for secure routes

---
+++ src/RequireAuth.tsx
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
+
+export const RequireAuth = () => {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = Cookies.get('token');
+
+    if (!token) {
+      navigate('/');
+    }
+  }, [navigate]);
+  
+};
