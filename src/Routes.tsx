4c02cca0dfc7c2b2bdf7f2f8b0d3f8249ed44cbc
ultr4 <cl.rch09@icloud.com>
2023-11-25 01:36:02 -0300

    :tada: Initial commit

---
+++ src/Routes.tsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import App from './App';
+
+
+
+function MainRoutes() {
+  return (
+    <Routes>
+      <Route element={<App />}>
+
+
+      </Route>
+    </Routes>
+  );
+}
+
+export default MainRoutes;
