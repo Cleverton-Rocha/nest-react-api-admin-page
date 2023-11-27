ca50be9a65c190bff6e19754fad65c8aef376534
ultr4 <cl.rch09@icloud.com>
2023-11-27 10:50:55 -0300

    :sparkles: feat: Created two new color tones for white

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'white-300': '#fdfdfc',
+        'white-500': '#efedea',
+      }
+    },
   },
   plugins: [],
-}
+};
