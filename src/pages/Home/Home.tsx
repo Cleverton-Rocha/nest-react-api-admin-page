99eaa8bbbb08ac15a79c8fde9241fc18260ba888
ultr4 <cl.rch09@icloud.com>
2023-11-28 14:49:01 -0300

    :lipstick: feat: Created responsive design for mobile

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -9,18 +9,18 @@ function Home() {
   return (
     <>
       <Header/>
-      <div className='flex justify-center mt-44 gap-10'>
+      <div className='flex flex-col md:flex-row md:justify-center items-center mt-44 mb-44 md:mb-0 gap-10'>
 
-        <div className='flex flex-col items-center rounded-sm w-1/6 h-1/5 p-5 border-2'>
-          <h1 className='text-4xl mt-5 font-semibold border-b'>Products</h1>
-          <span className='mt-5 text-sm font-medium'>Manage the products.</span>
-          <Link to='/products' className='mt-5 w-44 bg-blue-500 hover:bg-blue-600 transition duration-150 text-white font-medium rounded-sm text-center'>Go</Link>
+        <div className='flex flex-col items-center rounded-sm w-4/6 md:w-1/6 h-1/5 p-5 border-2'>
+          <h1 className='text-xl md:text-4xl mt-5 font-semibold border-b'>Products</h1>
+          <span className='mt-5 text-xs md:text-sm font-medium'>Manage the products.</span>
+          <Link to='/products' className='mt-5 w-44 bg-blue-500 hover:bg-blue-600 transition duration-150 text-white font-medium rounded-sm text-center text-sm md:text-lg'>Go</Link>
         </div>
 
-        <div className='flex flex-col items-center rounded-sm w-1/6 h-1/5 p-5 border-2'>
-          <h1 className='text-4xl mt-5 font-semibold border-b'>Users</h1>
-          <span className='mt-5 text-sm font-medium'>Manage the users.</span>
-          <Link to='/users' className='mt-5 w-44 bg-blue-500 hover:bg-blue-600 transition duration-150 text-white font-medium rounded-sm text-center'>Go</Link>
+        <div className='flex flex-col items-center rounded-sm w-4/6 md:w-1/6 h-1/5 p-5 border-2'>
+          <h1 className='text-xl md:text-4xl mt-5 font-semibold border-b'>Users</h1>
+          <span className='mt-5 text-xs md:text-sm font-medium'>Manage the users.</span>
+          <Link to='/users' className='mt-5 w-44 bg-blue-500 hover:bg-blue-600 transition duration-150 text-white font-medium rounded-sm text-center text-sm md:text-lg'>Go</Link>
         </div>
 
       </div>
