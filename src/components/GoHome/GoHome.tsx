c0658bb0d49f4cb53bf13a61cef0cfe29c752a06
ultr4 <cl.rch09@icloud.com>
2023-11-28 14:03:56 -0300

    :recycle: refactor: Changed width to w-28

--- src/components/GoHome/GoHome.tsx
+++ src/components/GoHome/GoHome.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function GoHome() {
   return (
-    <div className='flex p-2 rounded-sm hover:text-blue-600 transition duration-150'>
+    <div className='flex w-28 px-1 rounded-sm hover:text-blue-700 transition duration-150'>
       <MdChevronLeft size={25}/>
       <Link to={'/home'} className='font-medium' >Go home</Link>
     </div>
