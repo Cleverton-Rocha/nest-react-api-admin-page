8f8ac06b0e53205c45588db92e644af7913684cc
ultr4 <cl.rch09@icloud.com>
2023-12-02 03:17:38 -0300

    :sparkles: feat: Added routes Products and Product/id

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,6 +3,8 @@ import App from './App';
 import Login from './pages/Login/Login';
 import Home from './pages/Home/Home';
 import Users from './pages/Users/Users';
+import Products from './pages/Products/Products';
+import ProductPage from './pages/ProductPage/ProductPage';
 
 
 
@@ -10,9 +12,11 @@ function MainRoutes() {
   return (
     <Routes>
       <Route element={<App />}>
-        <Route path='/' element={<Login/>}/>
-        <Route path='home' element={<Home/>}/>
-        <Route path='users' element={<Users/>}/>
+        <Route path='/' element={<Login />} />
+        <Route path='home' element={<Home />} />
+        <Route path='users' element={<Users />} />
+        <Route path='products' element={<Products />} />
+        <Route path='product/:id' element={<ProductPage />} />
 
       </Route>
     </Routes>
